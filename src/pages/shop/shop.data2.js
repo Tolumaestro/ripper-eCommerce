41531b6296662d8da0cd343a74b9a64ff283893a
Maestro45 <tolulopesoetan1@gmail.com>
2022-04-10 12:12:53 +0100

    Updated shop data

--- src/pages/shop/shop.data2.js
+++ src/pages/shop/shop.data2.js
@@ -14,7 +14,7 @@ const SHOP_DATA = [
             {
                 id: 2,
                 name: 'Black Karma',
-                imageUrl: 'https://drive.google.com/uc?export=view&id=1MkVeVYnsRj2y1a3IBsPTeX3g8WCrRJOe',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1WVAupJEiLXl-txuuqp5VGHrnFLSqmpXW',
                 price: 3500
             },
             {
@@ -27,7 +27,31 @@ const SHOP_DATA = [
                 id: 4,
                 name: 'Follow your Dreams',
                 imageUrl: 'https://drive.google.com/uc?export=view&id=17VAnLE4NJZRKA1jc8S6g3g_QOILfTpAn',
-                price: 2000
+                price: 3000
+            },
+            {
+                id: 5,
+                name: 'Revolution Sweatshirt',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1Rxs8Y4G4nY7YdHRqDRArPUtsg2YAOz-J',
+                price: 5000
+            },
+            {
+                id: 6,
+                name: 'Hakuna Marijuana',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1VhhoygK-EvjAhEDs9cy7aj3gAf7o84lx',
+                price: 2500
+            },
+            {
+                id: 7,
+                name: "Dream Big",
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1c5yDxImaUoFjnjf62lf1E3kc1i366lRL',
+                price: 2500
+            },
+            {
+                id: 8,
+                name: "Make Some Trouble",
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1lSn0jvPnrsY9hYNEfejoh_kFIBy2E80Z',
+                price: 2500
             }
         ]
     },
@@ -58,7 +82,25 @@ const SHOP_DATA = [
                 id: 4,
                 name: 'Brown Cargo',
                 imageUrl: 'https://drive.google.com/uc?export=view&id=1myjMvHVy3rqNxSHiMC8GOWFVrsvCQvQt',
+                price: 8500
+            },
+            {
+                id: 5,
+                name: 'Black Sugar',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1gXQTu8j1VrkaZPe_bN-MCtjSKTF-2SAh',
                 price: 7000
+            },
+            {
+                id: 6,
+                name: 'Stone Cold',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1x1d_mTRvX2WDlIuzQhgTcYR6zWCEz44_',
+                price: 7500
+            },
+            {
+                id: 7,
+                name: 'Miami Beach',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1ne7fJznmKIZlL_ad00KuxceJL0fStv_W',
+                price: 5500
             }
         ]
     },
@@ -91,6 +133,86 @@ const SHOP_DATA = [
               imageUrl: 'https://drive.google.com/uc?export=view&id=1yBScTzLXkv4-uyUHXS76isWBuhXgv8pj',
               price: 2500
           },
+        ]
+    },
+    {
+        id: 4,
+        title: 'Womens',
+        routeName: 'womens',
+        items: [
+            {
+                id: 1,
+                name: 'Womens 1',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1a8T1SprO5RNvlHqVDG6kATobH7eYwHIR',
+                price: 12500 
+            },
+            {
+                id: 2,
+                name: 'Womens 2',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1PKs4I4DR2L44A51E7DCaVv5gmVczbwZ-',
+                price: 10000 
+            },
+            {
+                id: 3,
+                name: 'Womens 3',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1alDechd7cz0JvuB4Gwe7qSnZ4Hvjv4vi',
+                price: 9500 
+            },
+            {
+                id: 4,
+                name: 'Womens 4',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1dJv7xGV3udXz3SeLwV9zyTWlexGdbBXx',
+                price: 9000 
+            },
+            {
+                id: 5,
+                name: 'Womens 5',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1MkVeVYnsRj2y1a3IBsPTeX3g8WCrRJOe',
+                price: 9000 
+            },
+        ]
+    },
+    {
+        id: 5,
+        title: 'Mens',
+        routeName: 'mens',
+        items: [
+            {
+                id: 1,
+                name: 'Mens 1',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1-WkRgdJd0wPPzZY31ZcwZfPySKwTn2pZ',
+                price: 12500 
+            },
+            {
+                id: 2,
+                name: 'Mens 2',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1K0UcGanjHy786kQCkACiOMJz32wMbvXN',
+                price: 12500 
+            },
+            {
+                id: 3,
+                name: 'Mens 3',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1WORdbkMMDYHkpmTR6BTu6NHPOgSGDCIn',
+                price: 9500 
+            },
+            {
+                id: 4,
+                name: 'Mens 4',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1OE93CBhyPETMp6lmaQz-ZrpIRkEotcpO',
+                price: 9500 
+            },
+            {
+                id: 5,
+                name: 'Mens 5',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1mKLizKv4LZ5X544z5ldjjqZtX-N40Fo8',
+                price: 10500 
+            },
+            {
+                id: 6,
+                name: 'Mens 6',
+                imageUrl: 'https://drive.google.com/uc?export=view&id=1NHBzIC_tK90luEtZxEw6xWDwskRx-wGi',
+                price: 10500 
+            },
         ]
     }
 ]    

442d84cfb9d5941e62707d47f0203fd71e161e1b
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-07-14 13:43:32 +0100

    tried pwa but niggling issues

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -27,5 +27,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorkerRegistration.unregister();
+serviceWorker.register();
 reportWebVitals();
